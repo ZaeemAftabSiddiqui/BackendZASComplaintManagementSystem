8d04c6c49f5ee977426cfab6a8bbdd69a6a969c9
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-02-16 15:54:29 +0500

    registers user and save user info to database by async await

--- routes/auth.js
+++ routes/auth.js
@@ -7,30 +7,22 @@ router.get("/", (req, res) => {
   res.send("hello");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
 
   if (!name || !email || !phone || !work || !password || !cpassword) {
-    return res.status(422).json({ error: "fill all field" });
+    return res.status(422).json({ error: "fill all fields" });
   }
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "email already exist" });
-      }
-      const user = new User({ name, email, phone, work, password, cpassword });
-
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "user signin successfully" });
-        })
-        .catch((err) =>
-          res.status(500).json({ error: "Failed to registered" })
-        );
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "email already exist" });
+    }
+    const user = new User({ name, email, phone, work, password, cpassword });
+    await user.save();
+    res.status(201).json({ message: "user signin successfully" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 module.exports = router;
